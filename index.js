1034c83e8d2aead541c5589fa16c42f9e72dc29f
Prasad-Ayush <ayushprasad2163@gmail.com>
2024-07-07 03:19:40 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 3000;
+var userDet = null;
 
 // Connecting to MongoDB
 mongoose.connect('mongodb://localhost:27017/');
@@ -12,15 +12,15 @@ mongoose.connect('mongodb://localhost:27017/');
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
-    hiscore:{type: Number, default:0}
+    hiscore: { type: Number, default: 0 }
 });
-
 const User = mongoose.model('User', userSchema);
 
 //built in middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.use(express.static('templates'))
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
@@ -28,58 +28,117 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/snake-game',(req,res)=>{
-    res.sendFile('templates/index.html',{root:__dirname});
-})
-
+//Login
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
         // Check if user exists in MongoDB
-        const user = await User.findOne({ username });
-
+        const user = await User.findOne({ username: username });
         if (!user) {
             return res.send('User not found. Please sign up.');
         }
-
         // Validate password
-        if (user.password !== password) {
+        const isAuth = await bcrypt.compare(password, user.password);
+        if (!isAuth) {
             return res.send('Incorrect password.');
         }
-
+        
         // Successful login
-        // redirect to game page
-        return res.redirect('/snake-game');
+        userDet = user;
+        res.status(202).redirect('/snake-game');
     } catch (error) {
         console.error('Error during login:', error);
-        res.send('Internal server error.');
+        res.status(500).send('Internal server error.');
     }
 });
 
+//Signup
 app.post('/signup', async (req, res) => {
     const { username, password } = req.body;
 
     try {
         // Check if user already exists
-        const existingUser = await User.findOne({ username });
-
+        const existingUser = await User.findOne({ username: username });
         if (existingUser) {
             return res.send('Username already taken. Please choose another.');
         }
 
         // Create a new user
-        const newUser = new User({ username, password });
-        await newUser.save();
+        const hash = await bcrypt.hash(password, 10)
+        const newUser = new User({
+            username,
+            password: hash
+        });
+        console.log(newUser);
+        await newUser.save()
 
         // Successful signup
         // Redirect to login page 
-        res.redirect('/index.html');
+        res.status(201).redirect('/index.html');
     } catch (error) {
         console.error('Error during signup:', error);
-        res.send('Internal server error.');
+        res.status(500).send('Internal server error.');
     }
 });
+
+//Middleware
+const auth = ((req, res, next) => {
+    if (userDet === null) {
+        res.status(401).send('You are Unauthorized!');
+    }
+    else {
+        console.log('Authorized');
+        next();
+    }
+})
+
+app.get('/snake-game', auth, (req, res) => {
+    res.sendFile('templates/index.html', { root: __dirname });
+})
+
+//Get Data
+app.get('/snake-game/get-data', async (req, res) => {
+    try {
+        const doc = await User.findOne({ username: userDet.username }, 'username hiscore');
+        res.status(200).json(doc);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+//Update Data
+app.patch('/snake-game/:un/:hs', async (req, res) => {
+    const un = req.params.un;
+    const hs = +req.params.hs;
+    try {
+        const doc = await User.findOneAndUpdate({ username: un }, { hiscore: hs }, { new: true })
+        if (!doc) {
+            // Handle case where user is not found
+            return res.status(404).json({ error: 'User not found' });
+        }
+        console.log('updated');
+        res.json({ hiscore: doc.hiscore });
+    }
+    catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+})
+
+//Logout
+app.get('/snake-game/logout', (req, res) => {
+    try {
+        userDet = null;
+        res.redirect('/');
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(404);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- templates/snake.js
+++ templates/snake.js
@@ -1,17 +1,46 @@
-let start={x:0, y:0};
-let eatSound= new Audio('assets/music/eat.mp3');
-let overSound=new Audio('assets/music/over.mp3');
-let turnSound=new Audio('assets/music/turn.mp3');
-let snakeArr =[{x:13, y:15}]
-let speed=12 ;
+let start = { x: 0, y: 0 };
+let eatSound = new Audio('assets/music/eat.mp3');
+let overSound = new Audio('assets/music/over.mp3');
+let turnSound = new Audio('assets/music/turn.mp3');
+let snakeArr = [{ x: 13, y: 15 }]
+let speed = 10;
 let score = 0;
-let hiscoreval=0;
+let hiscoreval = 0;
 let lastPaintTime = 0;
-let food = {x: 18, y: 8};
+let food = { x: 12, y: 13 };
 
-function main(timestamp){
+fetch('/snake-game/get-data')
+    .then(response => response.json())
+    .then(data => {
+        const un = data.username;
+        const hs = data.hiscore;
+        localStorage.setItem('username', JSON.stringify(un));
+        hiscoreval = hs;
+        hiscoreBox.innerHTML = "Hi Score: " + hiscoreval;
+    })
+    .catch(e => {
+        console.log(e);
+    })
+
+async function updateScore(score) {
+    let un = localStorage.getItem('username');
+    let obj = JSON.parse(un);
+    const response = await fetch('/snake-game/' + obj + '/' + score, {
+        method: 'PATCH'
+    });
+    const doc = await response.json();
+}
+
+const log = document.getElementById('logout');
+log.addEventListener('click', function () {
+    localStorage.removeItem('username');
+    window.location.href = '/snake-game/logout';
+
+})
+
+function main(timestamp) {
     window.requestAnimationFrame(main);
-    if((timestamp-lastPaintTime)/1000 < 1/speed){
+    if ((timestamp - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
     lastPaintTime = timestamp;
@@ -20,64 +49,66 @@ function main(timestamp){
 
 function isCollide(snake) {
     // Snake collides with itself
-    for (let i = 1; i <snake.length; i++) {
-        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
             return true;
         }
     }
     // Snake collides with the wall
-    if(snake[0].x+start.x >18 || snake[0].x+start.x <1 || snake[0].y +start.y>18 || snake[0].y+start.y <1){
+    if (snake[0].x + start.x > 18 || snake[0].x + start.x < 1 || snake[0].y + start.y > 18 || snake[0].y + start.y < 1) {
         return true;
     }
-        
+
     return false;
 }
 
-function gameEngine(){
+function gameEngine() {
     //If snake collides
-    if(isCollide(snakeArr)){
+    if (isCollide(snakeArr)) {
         overSound.play();
-        start =  {x: 0, y: 0}; 
-        alert("Game Over. Press any key to play again!");
-        snakeArr = [{x: 13, y: 15}];
-        score = 0; 
+        start = { x: 0, y: 0 };
+        alert("Game Over. Press Enter to play again!");
+        snakeArr = [{ x: 13, y: 15 }];
+        score = 0;
         scoreBox.innerHTML = "Score: " + score;
     }
 
     //If snake eats the food
-    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
+    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
         eatSound.play();
         score += 1;
-        if(score>hiscoreval){
+        if (score > hiscoreval) {
             hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+
+            updateScore(score);
+            // localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
             hiscoreBox.innerHTML = "Hi Score: " + hiscoreval;
         }
         scoreBox.innerHTML = "Score: " + score;
-        snakeArr.unshift({x: snakeArr[0].x + start.x, y: snakeArr[0].y + start.y});
+        snakeArr.unshift({ x: snakeArr[0].x + start.x, y: snakeArr[0].y + start.y });
         let a = 1;
         let b = 18;
-        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
     }
 
     // Moving the snake
-    for (let i = snakeArr.length - 2; i>=0; i--) { 
-        snakeArr[i+1] = {...snakeArr[i]};//to avoid referncing of objects// new object is created
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] };//to avoid referncing of objects// new object is created
     }
 
     snakeArr[0].x += start.x;
     snakeArr[0].y += start.y;
-    
+
     //Displaying snake and food
-    board.innerHTML="";
-    snakeArr.forEach((e,index)=>{
-        snakeElement=document.createElement('div');
-        snakeElement.style.gridRowStart=e.y;
-        snakeElement.style.gridColumnStart=e.x;
-        if(index === 0){
+    board.innerHTML = "";
+    snakeArr.forEach((e, index) => {
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        if (index === 0) {
             snakeElement.classList.add('head');
         }
-        else{
+        else {
             snakeElement.classList.add('snake');
         }
         board.appendChild(snakeElement);
@@ -90,20 +121,9 @@ function gameEngine(){
 
 }
 
-//Setting High Score
-let hiscore = localStorage.getItem("hiscore");
-if(hiscore === null){
-    hiscoreval = 0;
-    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
-}
-else{
-    hiscoreval = JSON.parse(hiscore);
-    hiscoreBox.innerHTML = "Hi Score: " + hiscoreval;
-}
-
 // User Inputs
 window.requestAnimationFrame(main);
-window.addEventListener('keydown', (e) =>{
+window.addEventListener('keydown', (e) => {
     turnSound.play();
     switch (e.key) {
         case "ArrowUp":
